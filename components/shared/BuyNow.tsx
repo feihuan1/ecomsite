02a4396535b4edfbf3bb4b68aaa6264a26687725
eric <you@example.comericrealtor1997@gmail.com>
2023-12-23 02:56:23 -0800

    add buy now

---
+++ components/shared/BuyNow.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { urlFor } from "@/lib/sanity";
+import { ProductCart } from "@/lib/types";
+import { useShoppingCart } from "use-shopping-cart";
+
+export default function BuyNow({
+  currency,
+  description,
+  image,
+  name,
+  price,
+  price_id,
+}: ProductCart) {
+  const { checkoutSingleItem } = useShoppingCart();
+
+  function buyNow(priceId: string) {
+    checkoutSingleItem(priceId);
+  }
+
+  const product = {
+    name: name,
+    description: description,
+    price: price,
+    currency: currency,
+    image: urlFor(image).url(),
+    price_id: price_id,
+  };
+  return (
+    <Button
+      variant="outline"
+      onClick={() => {
+        buyNow(product.price_id);
+      }}
+    >
+      Checkout Now
+    </Button>
+  );
+}
